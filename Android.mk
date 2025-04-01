a2835f9304fb448226aa27c3a415b4476b68ad42
Michael Bestas <mkbestas@lineageos.org>
2025-04-01 21:26:58 +0000

    libstagefrighthw: Convert to blueprint
    Change-Id: I8ed83540ab990642d0956f320582a5a12cd7c207

--- Android.mk
+++ Android.mk
@@ -3,7 +3,6 @@ ifeq ($(call my-dir),$(call project-path-for,qcom-media))
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
-include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 
