ce821c1f85671b88e6778c6cb8ae2521896d3c49
Naseer Ahmed <naseer@codeaurora.org>
2025-04-02 20:38:10 +0000

    sdm: Cleanup unused code, round 2
    CRs-Fixed: 2078631
    Change-Id: I81ecf09f2aab346bd4b63737cf418e3b59a1483c

--- Android.mk
+++ Android.mk
@@ -18,7 +18,6 @@ LOCAL_MODULE := display_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
     $(display_top)/libcopybit \
     $(display_top)/libexternal \
-    $(display_top)/libgralloc \
     $(display_top)/libhdmi \
     $(display_top)/libhwcomposer \
     $(display_top)/liboverlay \
